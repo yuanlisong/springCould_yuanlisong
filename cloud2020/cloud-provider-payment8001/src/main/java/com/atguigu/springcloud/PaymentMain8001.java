b92b2af470402246c2c669f0b45674f11f0e3ef0
袁立松 <53557403+yuanlisong@users.noreply.github.com>
2021-02-23 23:08:22 +0800

    Create PaymentMain8001.java
    模块cloud-provider-payment8001主启动类
    
    Co-Authored-By: 袁立松 <53557403+yuanlisong@users.noreply.github.com>

---
+++ cloud2020/cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/PaymentMain8001.java
@@ -0,0 +1,18 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName: PaymentMain8001
+ * @Description: 主启动类
+ * @author: YuanLiSong
+ * @date: 2021/2/23 23:03
+ * @Copyright: YuanLiSong
+ */
+@SpringBootApplication
+public class PaymentMain8001 {
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentMain8001.class,args);
+	}
+}
