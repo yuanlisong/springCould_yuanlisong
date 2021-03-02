d1381773c4a3c7bd9d097d54727b39d44b2f3d2c
袁立松 <53557403+yuanlisong@users.noreply.github.com>
2021-03-02 15:07:07 +0800

    Update PaymentController.java
    create方法参数新增@RequestBody注解

--- cloud2020/cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
+++ cloud2020/cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
@@ -26,7 +27,7 @@ public class PaymentController {
 	private PaymentService paymentService;
 
 	@PostMapping(value = "/payment/create")
-	public CommonResult create(Payment payment){
+	public CommonResult create(@RequestBody Payment payment){
 		int result = paymentService.create(payment);
 		log.info("=====插入结果=====:"+result);
 		if (result > 0) {
